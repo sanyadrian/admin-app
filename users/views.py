1eee35d412730014e2f04bd19da7e2b73b52d190
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-21 22:16:17 -0400

    Created User model

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
 
-# Create your views here.
+@api_view(["GET"])
+def users(request):
+    users = User.objects.all()
+    return Response(users)
