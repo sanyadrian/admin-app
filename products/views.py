45fcac38ae6eb60abccf96a129816316eea7b807
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-26 21:19:24 -0400

    Created product serializer and product view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from users.authentication import JWTAuthentication
+from .models import Product
+from .serializers import ProductSerializers
+from admin.pagination import CustomPagination
 
-# Create your views here.
+
+class ProductGenericAPIView(
+    generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+    pagination_class = CustomPagination
+
+    def get(self, request, pk=None):
+        if pk:
+            return Response({
+                "data": self.retrieve(request, pk).data,
+            })
+        return self.list(request)
+
+    def post(self, request):
+        return Response({
+            "data": self.create(request).data
+        })
+    
+    def put(self, request, pk=None):
+        return Response({
+            "data": self.partial_update(request, pk).data
+        })
+
+    def delete(self, request, pk=None):
+        return self.destroy(request, pk)
