deaf0165deac7a5cbf6a4fdc6067ce2e9cb9d253
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-26 21:37:23 -0400

    Added uploading images features

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from users.authentication import JWTAuthentication
 from .models import Product
 from .serializers import ProductSerializers
@@ -41,3 +44,16 @@ class ProductGenericAPIView(
 
     def delete(self, request, pk=None):
         return self.destroy(request, pk)
+
+
+class FileUploadView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser, )
+    def post(self, request):
+        file = request.FILES["image"]
+        file_name = default_storage.save(file.name, file)
+        url = default_storage.url(file_name)
+        return Response({
+            "url": "http://localhost:8000/api" + url
+        })
